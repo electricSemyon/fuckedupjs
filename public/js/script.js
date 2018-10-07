b56287daa7e6bb95f93f47b2a9c38edb818201e3
electricSemyon <electricSemyon@gmail.com>
2018-10-07 20:06:12 +0300

    rewritten slider logic, so that we can add activities without modifying printActivities body

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 // У меня не очень хорошо с фантазией:
-const activities = ['Обоссать тебе ебало', 'Может быть расчехлить свои IDE', 'Сверстать cтраничку', 'Подключить жикваери'];
+const activities = ['Обоссать тебе ебало', 'Может быть расчехлить свои IDE', 'Вывести "хуй" в консоль', 'Сверстать cтраничку', 'Подключить жикваери'];
 const slider = document.querySelector('.development__range');
 let output = document.querySelector('.development__result');
 const list = document.querySelector('.development__activities-list');
@@ -10,7 +10,7 @@ output.textContent = `${slider.value}`
 
 slider.addEventListener('input', () => {
   output.textContent = slider.value;
-  list.innerHTML = printActivities().reduce((html, activity) => {
+  list.innerHTML = printActivities(slider, activities).reduce((html, activity) => {
     return html + `<li class="development__list-element">${activity}</li>`;
   }, '');
   if (slider.value >= 80) {
@@ -23,21 +23,23 @@ slider.addEventListener('input', () => {
 });
 
 // че, пацаны, клинкод?
-const printActivities = () => {
-  const val = slider.value;
-  let out = [];
-  if (val < 50) {
-    out = activities.slice(0, 1);
-  } else if (val >= 50 && val < 75) {
-    out = activities.slice(0,2);
-  } else if (val >= 75 && val < 90) {
-    out = activities.slice(0,3)
-  } else {
-    out = activities;
-  }
-  return out;
+const ACTIVITY_CHANGE_THRESHOLD = 50
+
+const printActivities = (slider, activities) => {
+  const { value, max } = slider
+  const activitiesLength = activities.length
+  const activityChangeStep = getActivityChangeStep(max, activitiesLength)
+  const rowIndex = getRowIndex(value, activityChangeStep)
+  const indexToSlice = keepInRange(1, activitiesLength, rowIndex)
+
+  return activities.slice(0, indexToSlice)
 }
 
+const getActivityChangeStep = (max, activitiesLength) =>
+  (max - ACTIVITY_CHANGE_THRESHOLD) / activitiesLength
 
+const getRowIndex = (value, activityChangeStep) =>
+  Math.trunc((value - ACTIVITY_CHANGE_THRESHOLD ) / activityChangeStep)
 
-
+const keepInRange = (from, to, number) =>
+  Math.min(Math.max(parseInt(number), from), to);
